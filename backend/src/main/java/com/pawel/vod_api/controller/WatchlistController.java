0ffdc4c0b8c52e72783ea4c1a2fbd1f833370a90
pawelwielewickidev <pawel.wielewicki.dev@gmail.com>
2026-03-23 10:15:22 +0100

    feat: implement GET endpoint for user watchlist

--- backend/src/main/java/com/pawel/vod_api/controller/WatchlistController.java
+++ backend/src/main/java/com/pawel/vod_api/controller/WatchlistController.java
@@ -6,23 +6,27 @@ import com.pawel.vod_api.service.WatchlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users/{userId}/profiles/{profileId}")
 @RequiredArgsConstructor
 public class WatchlistController {
     private final WatchlistService watchlistService;
 
-    @PostMapping("/users/{userId}/profiles/{profileId}/watchlists/{movieId}")
+    @PostMapping("/watchlists/{movieId}")
     public ResponseEntity<WatchlistResponseDto> addMovieToWatchlist(
             @PathVariable Long userId, @PathVariable Long profileId, @PathVariable Long movieId) {
 
         WatchlistResponseDto watchlistResponseDto = watchlistService.addToWatchlist(userId, profileId, movieId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(watchlistResponseDto);
+    }
+
+    @GetMapping("/watchlists")
+    public List<WatchlistResponseDto> getWatchlists(@PathVariable Long userId, @PathVariable Long profileId) {
+        return watchlistService.getWatchlist(userId, profileId);
     }
 }
