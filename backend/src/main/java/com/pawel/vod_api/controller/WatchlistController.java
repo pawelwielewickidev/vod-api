935621a8eeeb694b33c1d05bd6dc0935191f585b
pawelwielewickidev <pawel.wielewicki.dev@gmail.com>
2026-03-23 09:31:46 +0100

    feat:implement POST endpoint for adding movies to watchlist

--- backend/src/main/java/com/pawel/vod_api/controller/WatchlistController.java
+++ backend/src/main/java/com/pawel/vod_api/controller/WatchlistController.java
@@ -1,6 +1,13 @@
 package com.pawel.vod_api.controller;
 
+import com.pawel.vod_api.dto.WatchlistResponseDto;
+import com.pawel.vod_api.model.Watchlist;
+import com.pawel.vod_api.service.WatchlistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class WatchlistController {
+    private final WatchlistService watchlistService;
+
+    @PostMapping("/users/{userId}/profiles/{profileId}/watchlists/{movieId}")
+    public ResponseEntity<WatchlistResponseDto> addMovieToWatchlist(
+            @PathVariable Long userId, @PathVariable Long profileId, @PathVariable Long movieId) {
+
+        WatchlistResponseDto watchlistResponseDto = watchlistService.addToWatchlist(userId, profileId, movieId);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(watchlistResponseDto);
+    }
 }
--- backend/src/test/java/com/pawel/vod_api/controller/WatchlistControllerTest.java
+++ backend/src/test/java/com/pawel/vod_api/controller/WatchlistControllerTest.java
@@ -1,11 +1,20 @@
 package com.pawel.vod_api.controller;
 
+import com.pawel.vod_api.dto.*;
+import com.pawel.vod_api.model.Movie;
 import com.pawel.vod_api.service.WatchlistService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WatchlistController.class)
 public class WatchlistControllerTest {
@@ -13,9 +22,40 @@ public class WatchlistControllerTest {
     private MockMvc mockMvc;
     @MockitoBean
     private WatchlistService watchlistService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void shouldAddMovieToWatchlist() throws Exception{
+        MovieResponseDto movie = new MovieResponseDto();
+        movie.setId(1L);
+        movie.setTitle("Test Movie");
+
+        UserResponseDto user = new UserResponseDto();
+        user.setId(1L);
+
+        ProfileResponseDto profile = new ProfileResponseDto();
+        profile.setId(1L);
+        profile.setProfileName("Test Profile");
+
+        WatchlistResponseDto watchlist = new WatchlistResponseDto();
+        watchlist.setId(1L);
+        watchlist.setMovieTitle(movie.getTitle());
+        watchlist.setProfileName(profile.getProfileName());
+
+        Mockito.when(watchlistService.addToWatchlist(1L, 1L, 1L)).thenReturn(watchlist);
+
+        mockMvc.perform(post("/api/users/1/profiles/1/watchlists/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(watchlist)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.profileName").value("Test Profile"))
+                .andExpect(jsonPath("$.movieTitle").value("Test Movie"));
+
+
+
+
+
 
     }
 }
